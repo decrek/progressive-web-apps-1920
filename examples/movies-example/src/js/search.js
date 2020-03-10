6674f7cbb7e76cc030fe9aa30cd2c3bf14dbcd2e
Declan Rek <declanrek@gmail.com>
2020-03-10 11:19:52 +0100

    add debounce for input event

--- examples/movies-example/src/js/search.js
+++ examples/movies-example/src/js/search.js
@@ -1,5 +1,5 @@
 (function() {
-  document.querySelector('[data-search-input]').addEventListener('input', (e) => {
+  const onInput = debounce((e) =>  {
     const query = e.target.value
     const url = document.querySelector('[data-search-form]').getAttribute('action')
 
@@ -10,5 +10,27 @@
       .then(html => {
         document.querySelector('main').innerHTML = html
       })
-  })
+  }, 250)
+
+  document.querySelector('[data-search-input]').addEventListener('input', onInput)
+
+// From https://davidwalsh.name/javascript-debounce-function
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
 }())
